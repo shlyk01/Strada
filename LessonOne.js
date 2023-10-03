99a28c2e97a6b4626fdeef18dbbab1237b9ced1b
Evgeniy Shlyk <yoshinakakira@gmail.com>
2023-10-03 16:17:56 +0300

    verOne

---
+++ LessonOne.js
@@ -0,0 +1,26 @@
+// console.log(1 + '1') // (11) Автоматическое преобразование к строке 
+// console.log(true + true - true) // Автоматическое преобразование к числу, соответств. 1 + 1 - 1 = 1 
+// let a = 3;
+// let b = "4"
+// console.log(a + b) // (34) Автоматическое преобразование к строке
+// console.log(Number(a) + Number(b)) // (7) Явное преобразование к числовому типу
+// let a = 3;
+// let b = "4a" 
+// console.log(a + b) // (34a) Автоматическое преобразование к строке
+// console.log(Number(a) + Number(b)) // NaN\
+// console.log(Boolean(1)) // True Явное преобразование к Булевому типу
+// console.log(Boolean(0)) // false Явное преобразование к Булевому типу
+// console.log(String(123)) // "123" Явное преобразование к строке
+// console.log(Number(true)) // 1 Явное преобразование к числу
+// console.log(Boolean(1)) // (true) Явное преобразование к числу
+// console.log(Boolean('Hello')) // (true) Явное преобразование
+// console.log(Boolean(0)) // false 
+// console.log(Boolean('')) // false
+// console.log(Boolean(undefined)) // false
+// console.log(Boolean(null)) // false
+// console.log(Boolean(NaN)) // false 
+console.log(String(123))// '123' 
+console.log(String(true)) // 'true'
+console.log(String(undefined)) // 'undefined'
+console.log(String(null)) // 'null'
+console.log(String(NaN)) // 'NaN'
