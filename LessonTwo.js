e27cbdec3457e307a585e30facca38040e1ebd1e
Evgeniy Shlyk <yoshinakakira@gmail.com>
2023-10-03 18:54:16 +0300

    finalVer

--- LessonTwo.js
+++ LessonTwo.js
@@ -1,1 +1,18 @@
-console.log('Я' > 'a')
+// console.log(3 > 2) // true
+// console.log(4 == 2 * 2) // (4 = 4 ) true
+// console.log(6 >= 4) // true
+// console.log(4 <= 5) // true
+// console.log(5 != 4) // true
+// let result = 8 != 4;
+// console.log(result)
+// console.log('Кот' < 'кот') // true
+// console.log('01' == 1) // (1 == 1) true При сравнении всегда будет явное преобразование в числовой тип
+// console.log("2" > 1) // (2 > 1) true При сравнении всегда будет явное преобразование в числовой тип 
+// console.log(true == 1) // true явное преобразование из булевого типа в числовой
+// console.log('01'=== 1) // false При строгом равенстве оставляет строковый тип без попытки их преобразованя
+// console.log(true === 1) // false
+// console.log(true !== 1) // true
+console.log(null == undefined) // true это особое правило языка
+console.log(null > 0) // (0 > 0) false
+console.log(null == 0) // false
+console.log(null >= 0) // (0 == 0) true
